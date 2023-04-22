455a2d0273099df34f8e9c6e49a4a273be9c6d65
TziYoung <86063730+PaperGoat1210@users.noreply.github.com>
2023-04-22 18:10:01 +0800

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace GeometricFigures
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void PointMove(double dx, double dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+
+        public void PointRotate(double angle)
+        {
+            double rad = angle * Math.PI / 180;
+            double newX = X * Math.Cos(rad) - Y * Math.Sin(rad);
+            double newY = X * Math.Sin(rad) + Y * Math.Cos(rad);
+            newX = Math.Round(newX, 0);
+            newY = Math.Round(newY, 0);
+            X = newX;
+            Y = newY;
+        }
+    }
+
+    public class Line
+    {
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        public Line(Point startPoint, Point endPoint)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+
+        public void LineMove(double dx, double dy)
+        {
+            StartPoint.PointMove(dx, dy);
+            EndPoint.PointMove(dx, dy);
+        }
+
+        public void LineRotate(double angle)
+        {
+            StartPoint.PointRotate(angle);
+            EndPoint.PointRotate(angle);
+        }
+    }
+
+    public class Circle
+    {
+        public Point Center { get; set; }
+        public double Radius { get; set; }
+
+        public Circle(Point center, double radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public void CenterMove(double dx, double dy)
+        {
+            Center.PointMove(dx, dy);
+        }
+
+        public void CircleRotate(double angle)
+        {
+            Center.PointRotate(angle);
+        }
+    }
+
+    public class Aggregation
+    {
+        private List<object> figures;
+        public Aggregation()
+        {
+            figures = new List<object>();
+        }
+        public void AddFigure(object figure)
+        {
+            figures.Add(figure);
+        }
+        public void AggMove(double deltaX, double deltaY)
+        {
+            foreach (object figure in figures)
+            {
+                if (figure is Point point)
+                {
+                    point.PointMove(deltaX, deltaY);
+                }
+                else if (figure is Line line)
+                {
+                    line.LineMove(deltaX, deltaY);
+                }
+                else if (figure is Circle circle)
+                {
+                    circle.CenterMove(deltaX, deltaY);
+                }
+            }
+        }
+        public void AggRotate(double angle)
+        {
+            foreach (object figure in figures)
+            {
+                if (figure is Point point)
+                {
+                    point.PointRotate(angle);
+                }
+                else if (figure is Line line)
+                {
+                    line.LineRotate(angle);
+                }
+                else if (figure is Circle circle)
+                {
+                    circle.CircleRotate(angle);
+                }
+            }
+        }
+    }
+
+    class GeometricFigures
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(0, 1); // Create a point at (0, 1)
+            Point point2 = new Point(2, 3); // Create a point at (2, 3)
+            Console.WriteLine($"P1: ({point1.X}, {point1.Y})");
+            Console.WriteLine($"P2: ({point2.X}, {point2.Y})");
+
+            Line line1 = new Line(new Point(1, 2), new Point(3, 4)); // Create a line of P1(1, 1) and P2(2, 2)
+            Console.WriteLine($"Line P1: ({line1.StartPoint.X}, {line1.StartPoint.Y})");
+            Console.WriteLine($"Line P2: ({line1.EndPoint.X}, {line1.EndPoint.Y})");
+
+            Circle circle = new Circle(new Point(1, 2), 3); // Create a circle with center (1, 2) and radius 3
+            Console.WriteLine($"Circle center: ({circle.Center.X}, {circle.Center.Y})");
+            Console.WriteLine($"Circle radius: {circle.Radius}");
+
+            Aggregation agg = new Aggregation();
+            agg.AddFigure(point1);
+            agg.AddFigure(point2);
+            agg.AddFigure(line1);
+            agg.AddFigure(circle);
+
+            agg.AggMove(2, 2);
+            Console.WriteLine("\nMove (2,2)");
+            Console.WriteLine($"Point1: ({point1.X}, {point1.Y})");
+            Console.WriteLine($"Point2: ({point2.X}, {point2.Y})");
+            Console.WriteLine($"Line P1: ({line1.StartPoint.X}, {line1.StartPoint.Y})");
+            Console.WriteLine($"Line P2: ({line1.EndPoint.X}, {line1.EndPoint.Y})");
+            Console.WriteLine($"Circle center: ({circle.Center.X}, {circle.Center.Y})");
+
+            agg.AggRotate(90);
+            Console.WriteLine("\nRotate 90 Degrees");
+            Console.WriteLine($"Point1: ({point1.X}, {point1.Y})");
+            Console.WriteLine($"Point2: ({point2.X}, {point2.Y})");
+            Console.WriteLine($"Line P1: ({line1.StartPoint.X}, {line1.StartPoint.Y})");
+            Console.WriteLine($"Line P2: ({line1.EndPoint.X}, {line1.EndPoint.Y})");
+            Console.WriteLine($"Circle center: ({circle.Center.X}, {circle.Center.Y})");
+        }
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class PalindromeChecker
+{
+    static void Main()
+    {
+        string inputString1 = "a@b!!b$a";
+        string trashSymbolString1 = "!@$";
+
+        string inputString2 = "?Aa#c";
+        string trashSymbolString2 = "#?";
+
+        Console.WriteLine($"InputString: {inputString1}");
+        Console.WriteLine($"TrashSymbolsString: {trashSymbolString1}");
+        if (IsPalindrome(inputString1, trashSymbolString1))
+        {
+            Console.WriteLine("Result should be: True");
+        }
+        else
+        {
+            Console.WriteLine("Result should be: False");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"InputString: {inputString2}");
+        Console.WriteLine($"TrashSymbolsString: {trashSymbolString2}");
+        if (IsPalindrome(inputString2, trashSymbolString2))
+        {
+            Console.WriteLine("Result should be: True");
+        }
+        else
+        {
+            Console.WriteLine("Result should be: False");
+        }
+    }
+
+    public static bool IsPalindrome(string InputString, string TrashSymbolString)
+    {
+        // Convert input and trash symbols to lowercase
+        InputString = InputString.ToLower();
+        TrashSymbolString = TrashSymbolString.ToLower();
+
+        int i = 0;
+        int j = InputString.Length - 1;
+
+        while (i < j)
+        {
+            // Skip trash symbols at the beginning of the string
+            while (i < InputString.Length && TrashSymbolString.Contains(InputString[i]))
+            {
+                i++;
+            }
+
+            // Skip trash symbols at the end of the string
+            while (j >= 0 && TrashSymbolString.Contains(InputString[j]))
+            {
+                j--;
+            }
+
+            // Compare characters ignoring trash symbols
+            if (i < j && InputString[i] != InputString[j])
+            {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+}
+
+
